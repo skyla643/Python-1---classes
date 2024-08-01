ca61733523d1b2a445d35a19e68cffac75f277f3
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2024-08-01 19:31:00 -0400

    Update Assignment 1 - real.py to fix variable conflict

--- AS-1.py - 8/1/24.py
+++
@@ -1,8 +0,0 @@
-# prompt to enter first name
-firstname = input("please enter your first name: ")
-# prompt to enter last name
-lastname = input("please enter your last name: ")
-# enter fav color
-color = input("please enter your  favorite color: ")
-# output
-print("Hello," + firstname + "," + lastname + "," + color + "!")
---
+++ Assignment 1 - real.py
@@ -0,0 +1,9 @@
+# input
+length = float(input("Enter a length: "))
+w = float(input("Enter a width: "))
+
+# processing
+area = length * w
+
+# output
+print("The area is: ", area)
